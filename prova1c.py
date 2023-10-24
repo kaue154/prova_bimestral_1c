04706ba80c6248afa6f6f22762d43c1f1aa789ad
kaue154 <144684890+kaue154@users.noreply.github.com>
2023-10-24 08:16:14 -0300

    oi

--- prova1c.py
+++ prova1c.py
@@ -1,3 +1,3 @@
 n=int(input('digite um numero'))#aqui vc vai digitar o numero
 s=n+1#aqui e a formula do sucessor
-print('o sucessor do numero e',s)#aqui e o resultado
+print('o sucessor do numero e',s)#aqui e o re
